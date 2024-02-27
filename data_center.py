2cbe427a59957d84e9434c7d44cedb9599621d16
Fabrice Ingabire <ingfabrice3@gmail.com>
2024-02-27 11:42:15 -0500

    add mininet

--- data_center.py
+++ data_center.py
@@ -57,7 +57,7 @@ class MyTopo( Topo ):
 def runExperiment():
     # create and test the emulator
     fan_out = sys.argv[1]
-    topo = MyTopo(fan_out)
+    topo = MyTopo(int(fan_out))
     net = Mininet(topo)
     net.start()
     print("Pinging all nodes")

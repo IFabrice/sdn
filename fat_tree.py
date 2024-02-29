6379d207898decb08063bfe4eb4f6b65c99bd819
Fabrice Ingabire <ingfabrice3@gmail.com>
2024-02-29 11:16:08 -0500

    fat_tree mid implementation

--- fat_tree.py
+++ fat_tree.py
@@ -6,22 +6,60 @@ import sys
 from mininet.net import Mininet
 from mininet.util import irange, dumpNodeConnections
 
+
+
 class MyTopo( Topo ):
 
-    def __init__(self):
+    def __init__(self, k, **opts):
 
         # Topology initialization
-        Topo.__init__(self)
+        Topo.__init__(self, **opts)
+        
+        self.k = k
+        self.core_switches = []
+        self.aggr_switches = []
+        self.edge_switches = []
+        self.all_hosts = []
 
+        # initialize core switches
+        for index in range(1, pow(k/2, 2) + 1):
 
-        self.addSwitch("c1", private_ip="10.0.0.2")
+            # i represent rows and j represent columns
+            i = index / (k/2)
+            j = index % (k/2)
 
-        self.addHost("h1")
-        self.addHost("h2")
-        self.addLink('c1', 'h1')
-        self.addLink('c1', 'h2')
+            # creating dpid in the form "00 : 00 : 00 : 00 : 00 : k : j : i"
+            dpid = "00:00:00:00:00:" + str(k) + ":"+ str(i) + str(j)
+
+            self.core_switches.append(self.addSwitch('c{}'.format(index), dpid))
 
         
+        for pod in range(0, k):
+            
+            # initialization of aggregate and edge switches
+            for index in range(0, k/2):
+
+                # initialization of pid of the form "00 : 00 : 00 : 00 : 00 : pod : switch : 01"
+                aggr_dpid = "00:00:00:00:00:" + str(pod) + ":" + str(index + k/2) + ":" + "01"
+                self.aggr_switches.append(self.addSwitch('a{}'.format(index + pod*k/2), aggr_dpid))
+
+                edge_dpid = "00:00:00:00:00:" + str(pod) + ":" + str(index) + ":" + "01"
+                self.edge_switches.append(self.addSwitch("e{}".format(index + pod*k/2), edge_dpid))
+
+                self.addEdgeHosts(pod, index)
+
+
+        
+    def addEdgeHosts(self, pod, switch):
+        
+        # initialize k hosts and link all of them to the current edge switch. Note: hosts ip is in format: "10.pod.switch.ID"
+        for index in range(self.k):
+            ip = "10.{}.{}.{}".format(pod, switch, index)
+            self.all_hosts.append(self.addHost("c{}".format(index), ip=ip))
+            self.addLink(self.edge_switches[-1], self.all_hosts[-1])
+
+
+
 
     def addLinks(self):
 
